8c754bdc0ee5cce4ec9d9ddce9c035457e223644
Tausi Ahmed <tausifahmed49@gmail.com>
2020-10-09 01:31:24 +0600

    html template done

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+; (function ($) {
+    $(document).ready(function () {
+        $(".testimonial-slides").slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: false,
+            loop: true,
+            dots: true,
+            arrows: false,
+        });
+    });
+}(jQuery));
